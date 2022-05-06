b4f53ab00b842346e1ef32b48a9d7c2cdffbc029
Fernando Santana <fernandosantanajr@gmail.com>
2022-05-06 00:21:50 -0300

    refactor: create controller inteface and make SignupControler sign it

--- src/presentetion/controllers/signup.ts
+++ src/presentetion/controllers/signup.ts
@@ -1,7 +1,8 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helpers'
+import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requireFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requireFields) {
