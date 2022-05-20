43afd663d1debabb0b08f0eb40cdb9fddbde4c46
Fernando Santana <fernandosantanajr@gmail.com>
2022-05-19 22:11:36 -0300

    feat: helper mongodb return collection

--- src/infra/db/mongodb/helpers/mongo-helpers.ts
+++ src/infra/db/mongodb/helpers/mongo-helpers.ts
@@ -1,14 +1,14 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
-  client: null as MongoClient,
+  client: null,
   async connect (url: string): Promise<void> {
-    this.client = await MongoClient.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    this.client = await MongoClient.connect(url)
   },
   async disconnect () {
     await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
