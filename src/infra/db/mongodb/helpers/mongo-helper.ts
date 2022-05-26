f7c235faa2db22166afbd3d0ddaa543d61840432
Fernando Santana <fernandosantanajr@gmail.com>
2022-05-25 22:35:57 -0300

    refactor: group envs on a env helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,15 +2,19 @@ import { Collection, MongoClient, InsertOneResult } from 'mongodb'
 
 export const MongoHelper = {
   client: null,
+
   async connect (url: string): Promise<void> {
     this.client = await MongoClient.connect(url)
   },
+
   async disconnect () {
     await this.client.close()
   },
+
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
   },
+
   map (orignalData: any, result: InsertOneResult) {
     const id = result.insertedId.toString()
     const object = { id, ...orignalData }
