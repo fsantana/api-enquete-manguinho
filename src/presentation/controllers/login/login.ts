2b19e9771e9dbb462656d264ab01948431368678
Fernando Santana <fernandosantanajr@gmail.com>
2022-06-02 23:47:43 -0300

    feat: ensture LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
 import { MissingParamError } from '../../errors'
 import { badRequest, ok } from '../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requireFields = ['email', 'password']
     for (const field of requireFields) {
@@ -10,6 +17,8 @@ export class LoginController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
+
+    this.emailValidator.isValid(httpRequest.body.email)
     return await new Promise(resolve => resolve(ok({})))
   }
 }
