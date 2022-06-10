06d0bea3fd014bee5339b7f30e3c2eddd0675aab
Fernando Santana <fernandosantanajr@gmail.com>
2022-06-10 09:19:22 -0300

    feat: ensure BcryptAdapter calls bcrypt compare with correct values

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,5 +1,5 @@
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcrypterAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
@@ -9,7 +9,7 @@ import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
-  const bcryptAdapter = new BcrypterAdapter(salt)
+  const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const logErrorRepository = new LogMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
